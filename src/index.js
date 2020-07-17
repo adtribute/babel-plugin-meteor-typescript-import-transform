f12633874c1e86112b63329b3f13ae799f2debcc
Phuc Nguyen <contact@phucnguyen.info>
2020-07-17 19:58:35 +0700

    Handle Meteor TypeScript import issues
    The plugin can now fix the following issues:
    
    - Paths without leading slash (e.g. `imports/bar`) is not recognized as valid
    import paths
    
    - Paths mapping in `tsconfig.json` is not recognized
    
    - `import 'imports/module/main'` may not work as expected without a
    file extension

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-const fs = require('fs');
-const { dirname, join } = require('path');
+const fs = require("fs");
+const path = require("path");
+const {
+  getTSFilePaths,
+  getTSPathsMapping,
+  removeLeadingAndTrailingSlash,
+  resolveRelativePath,
+} = require("./utils");
 
-const appDir = dirname(dirname(__dirname));
+const appDir = process.cwd();
 const hasOwn = Object.prototype.hasOwnProperty;
 
 // These directory names are computed only once per build process, so you
 // may need to restart Meteor to pick up any changes.
 
 const nodeModulesDirNames = Object.create(null);
-fs.readdirSync(join(appDir, 'node_modules')).forEach((dir) => {
-  if (!dir.startsWith('.')) {
+fs.readdirSync(path.join(appDir, "node_modules")).forEach((dir) => {
+  if (!dir.startsWith(".")) {
     nodeModulesDirNames[dir] = true;
   }
 });
 
 const topLevelDirNames = Object.create(null);
 fs.readdirSync(appDir).forEach((item) => {
-  if (!item.startsWith('.') && item !== 'node_modules') {
+  if (!item.startsWith(".") && item !== "node_modules") {
     const stat = fs.statSync(item);
     if (stat.isDirectory()) {
       topLevelDirNames[item] = stat;
@@ -24,45 +30,94 @@ fs.readdirSync(appDir).forEach((item) => {
   }
 });
 
-// Babel plugin that rewrites import declarations like
-//
-//   import foo from "imports/bar"
-//
-// to use properly absolute identifier strings like
-//
-//   import foo from "/imports/bar"
-//
-// TypeScript can understand imports/bar thanks to the "paths" property in
-// tsconfig.json, but Node and Meteor treat imports/bar as referring to a
-// package in node_modules called "imports", which does not exist.
-//
-// If a directory name exists in both node_modules and the root
-// application directory, the node_modules package will take precedence.
+const tsFilePaths = getTSFilePaths(`${appDir}/imports`);
+
+const pathsMapping = getTSPathsMapping(appDir);
 
 module.exports = function plugin(api) {
-  function helper(path) {
+  function helper(path, state) {
     // An ImportDeclaration will always have a source, but an
     // ExportAllDeclaration or ExportNamedDeclaration may not.
     const { source } = path.node;
     if (!source) return;
+    let resolvedSourceId = source.value;
+    let importPathFirstElement = resolvedSourceId.split("/", 1)[0];
 
-    const sourceId = source.value;
-    const name = sourceId.split('/', 1)[0];
+    // Skip npm module and Meteor package imports
+    if (
+      resolvedSourceId.startsWith("meteor/") ||
+      hasOwn.call(nodeModulesDirNames, importPathFirstElement)
+    )
+      return;
+
+    // Parse tsconfig.json to resolve paths mapping
+    if (
+      pathsMapping &&
+      Object.keys(pathsMapping).includes(importPathFirstElement)
+    ) {
+      resolvedSourceId = resolvedSourceId.replace(
+        importPathFirstElement,
+        `/${pathsMapping[importPathFirstElement]}`
+      );
+    }
+
+    // Resolve import paths not having an extension
+    const importedFileName = resolvedSourceId.split("/").pop();
+    if (!importedFileName.includes(".")) {
+      let resolvedRelativePath = resolvedSourceId;
+
+      // Resolve relative paths
+      if (
+        resolvedRelativePath.startsWith("./") ||
+        resolvedRelativePath.startsWith("../")
+      ) {
+        const { sourceFileName } = state.file.opts;
+        const currentPath = sourceFileName.substring(
+          0,
+          sourceFileName.lastIndexOf("/")
+        );
+        resolvedRelativePath = resolveRelativePath(
+          currentPath,
+          resolvedRelativePath
+        );
+      }
+
+      const matchingTSFilePath = tsFilePaths.find(
+        (filePath) =>
+          filePath ===
+            `${removeLeadingAndTrailingSlash(resolvedRelativePath)}.ts` ||
+          filePath ===
+            `${removeLeadingAndTrailingSlash(resolvedRelativePath)}.tsx`
+      );
+
+      if (matchingTSFilePath) {
+        resolvedSourceId = matchingTSFilePath.includes(".tsx")
+          ? `${resolvedRelativePath}.tsx`
+          : `${resolvedRelativePath}.ts`;
+        // Update this variable for the next processing stage
+        importPathFirstElement = resolvedSourceId.split("/", 1)[0];
+      }
+    }
 
     // If the first component of the sourceId is a top-level directory in
     // the application, and not the name of a directory in node_modules,
     // prepend a leading / to make it an absolute identifier that Meteor's
     // module system can understand.
+    // e.g. 'imports/foo/bar' --> '/imports/foo/bar'
     if (
-      hasOwn.call(topLevelDirNames, name) &&
-      !hasOwn.call(nodeModulesDirNames, name)
+      hasOwn.call(topLevelDirNames, importPathFirstElement) &&
+      !hasOwn.call(nodeModulesDirNames, importPathFirstElement)
     ) {
-      path.get('source').replaceWith(api.types.stringLiteral(`/${sourceId}`));
+      resolvedSourceId = `/${resolvedSourceId}`;
     }
+
+    // Perform path transform
+    if (resolvedSourceId !== source.value)
+      path.get("source").replaceWith(api.types.stringLiteral(resolvedSourceId));
   }
 
   return {
-    name: 'transform-non-relative-imports',
+    name: "meteor-typescript-import-transform",
     visitor: {
       ImportDeclaration: helper,
       ExportAllDeclaration: helper,
---
+++ src/utils.js
@@ -0,0 +1,89 @@
+const fs = require("fs");
+
+const removeLeadingAndTrailingSlash = function (path) {
+  return path.replace(/^\/|\/$/g, "");
+};
+
+// Generate an array of all .ts and .tsx file paths
+const getTSFilePaths = function (rootPath) {
+  const entries = fs.readdirSync(rootPath, { withFileTypes: true });
+  const tsFilePaths = entries
+    .filter((file) => {
+      if (!file.isFile()) return;
+
+      const fileExtension = file.name.split(".").pop();
+
+      return (
+        !file.name.endsWith(".d.ts") && ["ts", "tsx"].includes(fileExtension)
+      );
+    })
+    .map((file) => {
+      const importsPath = "imports/" + rootPath.split("imports/").pop();
+      return `${importsPath}/${file.name}`;
+    });
+  const folders = entries.filter((folder) => folder.isDirectory());
+
+  folders.forEach((folder) => {
+    tsFilePaths.push(...getTSFilePaths(`${rootPath}/${folder.name}`));
+  });
+
+  return tsFilePaths;
+};
+
+// Read tsconfig.json and parse paths mapping
+const getTSPathsMapping = function (rootPath) {
+  const tsConfigPath = `${rootPath}/tsconfig.json`;
+
+  if (!fs.existsSync(tsConfigPath)) return null;
+
+  const tsConfig = JSON.parse(fs.readFileSync(tsConfigPath));
+  const pathsMapping =
+    tsConfig && tsConfig.compilerOptions && tsConfig.compilerOptions.paths;
+  const resolvedPathsMapping = {};
+
+  if (!pathsMapping) return null;
+
+  Object.entries(pathsMapping).forEach(([key, [originalPath]]) => {
+    resolvedPathsMapping[key.replace("/*", "")] = originalPath.replace(
+      "/*",
+      ""
+    );
+  });
+
+  return resolvedPathsMapping;
+};
+
+// Resolve the provided relative part from the current path
+// e.g. resolveRelativePath('imports/foo/bar', '../stuff.ts')
+// --> 'imports/foo/stuff.ts'
+const resolveRelativePath = function (currentPath, relativePath) {
+  const isCurrentPath = relativePath.slice(0, 2) === "./";
+  const isParentPath = relativePath.slice(0, 3) === "../";
+  let output;
+
+  if (!isCurrentPath && !isParentPath) throw new Error("Invalid relative path");
+
+  const currentPathWithoutTrailingSlash = currentPath.replace(/\/$/, "");
+
+  if (isCurrentPath)
+    output = `${currentPathWithoutTrailingSlash}/${relativePath.replace(
+      "./",
+      ""
+    )}`;
+
+  if (isParentPath) {
+    const currentPathElements = currentPath.split("/");
+    currentPathElements.pop();
+    output = `${currentPathElements.join("/")}/${relativePath.replace(
+      "../",
+      ""
+    )}`;
+  }
+
+  return output;
+};
+
+exports.removeLeadingAndTrailingSlash = removeLeadingAndTrailingSlash;
+exports.resolveRelativePath = resolveRelativePath;
+exports.getTSFilePaths = getTSFilePaths;
+exports.getTSPathsMapping = getTSPathsMapping;
